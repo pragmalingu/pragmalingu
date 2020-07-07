9b709af8cc0e5139bcc0ad1008ea8e50ecf1c79a
Samy Ateia <samy@samyateia.de>
2020-07-07 15:25:08 +0200

    .gitignore fix

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/miriam/pragmalingu.github.io/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/miriam/pragmalingu.github.io/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/miriam/pragmalingu.github.io/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/miriam/pragmalingu.github.io/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
-  require("/Users/miriam/pragmalingu.github.io/src/css/custom.css"),
-];
